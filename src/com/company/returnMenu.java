9cfd61c76e9f59d392a47224b6fe7e5e2b7ef408
JuanVasUMG <82298149+JuanVasUMG@users.noreply.github.com>
2021-08-08 00:12:36 -0600

    Agregamos la clase returnMenu con el metodo backMenu para que el usuario decida si sale del programa o regresa al menu principal

---
+++ src/com/company/returnMenu.java
@@ -0,0 +1,22 @@
+package com.company;
+
+//Importamos la Libreria para leer entrada de Teclado
+import java.util.Scanner;
+
+public class returnMenu {
+    public static Boolean backMenu() {
+        Scanner in = new Scanner(System.in);
+        int option = 0;
+        System.out.println("\nDesea regresar al menu Principal: " +
+                "\n1. Si" +
+                "\n2. Salir del Programa" +
+                "\nElija una Opcion: ");
+        option = in.nextInt();
+        if (option==1) {
+            return true;
+        } else {
+            System.out.println("Saliendo del programa...");
+            return false;
+        }
+    }
+}
